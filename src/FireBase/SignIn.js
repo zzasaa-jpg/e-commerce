ea02712771ff52b24a5698b7b63740c0434640b4
zzasaa <zzsdrt354.com>
2024-04-11 16:45:26 +0530

    Summary of this commit 1.change the UI in Google and Signin page. 2.Updated the README.md file.

--- src/FireBase/SignIn.js
+++ src/FireBase/SignIn.js
@@ -7,6 +7,7 @@ import { MdOutlineEmail } from "react-icons/md";
 import { IoKeyOutline } from "react-icons/io5";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import logo from "../FireBase/logo.png"
 
 function SignIn() {
     const [error, setError] = useState(false);
@@ -87,39 +88,45 @@ function SignIn() {
         setShow(!show)
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         toast.info('Use the Dummy Email & Password.', {
             theme: 'colored'
         })
-    },[])
+    }, [])
 
     return (
         <div className='flex flex-col justify-center items-center h-screen p-2 bg-[#f2f1f7]'>
             <ToastContainer />
-            <div className='flex p-1'>
-                <h1 className='text-black'>{<h1 className='text-[10px] font-semibold'>{error && messageError}</h1>}</h1>
-            </div>
-            <div className='bg-white w-auto sm:w-[350px] md:w-[400px] lg:w-[600px] h-[380px] sm:h-[400px] md:h-[400px] lg:h-[400px] gap-1 flex flex-col items-center shadow-2xl rounded-[20px] p-2'>
-                <h1 className='text-[30px] sm:text-[35px] md:text-[38px] lg:text-[40px] text-center'>SignIn</h1>
-                {/* <h1>{error && messageError}</h1> */}
-                <form onSubmit={handleSignUpMicrosoft} className='flex flex-col gap-4 pt-6'>
-                    <div className='flex justify-center items-center'>
+            <main className='flex flex-col sm:flex-row md:flex-row lg:flex-row justify-center lg:items-center  gap-[5px] sm:gap-[10px] md:gap-[20px] lg:gap-[80px]'>
+                <section className='flex justify-center items-center'>
+                    <img src={logo} alt="logo" className='w-[100px] h-[100px] rounded-xl sm:w-[280px] sm:h-[280px] sm:rounded-2xl md:w-[300px] md:h-[300px] md:rounded-2xl lg:w-[400px] lg:h-[400px] lg:rounded-2xl shadow-2xl shadow-zinc-500' />
+                </section>
+                <section>
+                    <div className='flex p-1'>
+                        <h1 className='text-black'>{<h1 className='text-[10px] font-semibold'>{error && messageError}</h1>}</h1>
+                    </div>
+                    <div className='bg-white w-auto sm:w-[350px] md:w-[400px] lg:w-[600px] h-[380px] sm:h-[400px] md:h-[400px] lg:h-[400px] gap-1 flex flex-col items-center shadow-2xl rounded-[20px] p-2'>
+                        <h1 className='text-[30px] sm:text-[35px] md:text-[38px] lg:text-[40px] text-center'>SignIn</h1>
+                        {/* <h1>{error && messageError}</h1> */}
+                        <form onSubmit={handleSignUpMicrosoft} className='flex flex-col gap-4 pt-6'>
+                            <div className='flex justify-center items-center'>
 
-                        <input className='bg-transparent border-[1px] border-black p-2 rounded-[4px] text-[20px] w-full sm:w-[250px] md:w-[300px] lg:w-[350px]' placeholder='Enter the Emmail...' type="email" name="" id="email" onChange={handleEmail} /> <MdOutlineEmail className='
+                                <input className='bg-transparent border-[1px] border-black p-2 rounded-[4px] text-[20px] w-full sm:w-[250px] md:w-[300px] lg:w-[350px]' placeholder='Enter the Emmail...' type="email" name="" id="email" onChange={handleEmail} /> <MdOutlineEmail className='
             border-[1px] border-black h-12 w-12 rounded-[4px] ' />
-                    </div>
+                            </div>
 
-                    <div className='flex justify-center items-center'>
+                            <div className='flex justify-center items-center'>
 
-                        <input className='bg-transparent border-[1px] border-black p-2 rounded-[4px] text-[20px] w-full sm:w-[250px] md:w-[300px] lg:w-[350px]' placeholder='Enter the Password...' type={show ? "password": "text"} name="" id="password" onChange={handlePassword} /> <IoKeyOutline className='
+                                <input className='bg-transparent border-[1px] border-black p-2 rounded-[4px] text-[20px] w-full sm:w-[250px] md:w-[300px] lg:w-[350px]' placeholder='Enter the Password...' type={show ? "password" : "text"} name="" id="password" onChange={handlePassword} /> <IoKeyOutline className='
             border-[1px] border-black h-12 w-12 rounded-[4px] ' onMouseEnter={handleShow} onMouseLeave={handleShow} />
+                            </div>
+                            {/* <input className='bg-transparent border-[1px] border-black p-2 rounded-[4px] text-[20px]' type="password" name="" id="password" onChange={handlePassword}/> */}
+                            <button type='submit' className='cursor-pointer text-[20px] border-black border-[1px] hover:bg-[#efefef] w-full sm:w-[300px] md:w-[350px] lg:w-[400px] p-2 rounded-[4px]'>Signin</button>
+                        </form>
+                        <h1 className='pt-4 sm:pt-2 md:pt-2 lg:pt-2'>Already have an account <Link className='underline' to='/'>Login</Link></h1>
                     </div>
-                    {/* <input className='bg-transparent border-[1px] border-black p-2 rounded-[4px] text-[20px]' type="password" name="" id="password" onChange={handlePassword}/> */}
-                    <button type='submit' className='cursor-pointer text-[20px] border-black border-[1px] hover:bg-[#efefef] w-full sm:w-[300px] md:w-[350px] lg:w-[400px] p-2 rounded-[4px]'>Signin</button>
-                </form>
-                <h1 className='pt-4 sm:pt-2 md:pt-2 lg:pt-2'>Already have an account <Link className='underline' to='/'>Login</Link></h1>
-            </div>
-
+                </section>
+            </main>
         </div>
     )
 }
