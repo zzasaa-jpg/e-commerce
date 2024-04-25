7b084394f9f5b6e1a45ce89cbe193c406fa1ee24
zzasaa <zzsdrt354.com>
2024-04-25 16:23:21 +0530

    Google Authentication and Signin UI Changed.

--- src/FireBase/GoogleAuthentication.js
+++ src/FireBase/GoogleAuthentication.js
@@ -8,7 +8,8 @@ import { MdOutlineEmail } from "react-icons/md";
 import { IoKeyOutline } from "react-icons/io5";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import logo from "../FireBase/logo.png"
+import logo from "../FireBase/logo.png";
+import Typewriter from "typewriter-effect";
 
 function GoogleAuthentication() {
     const [error, setError] = useState(false);
@@ -158,42 +159,65 @@ function GoogleAuthentication() {
     }
 
     return (
-        <div className='flex flex-col justify-center h-screen bg-[#f2f1f7] p-2'>
-            <ToastContainer />
-            <main className='flex flex-col sm:flex-row md:flex-row lg:flex-row justify-center lg:items-center  gap-[5px] sm:gap-[10px] md:gap-[20px] lg:gap-[80px]'>
-                <section className='flex justify-center items-center'>
-                    <img src={logo} alt="logo" className='hidden sm:w-[280px] sm:h-[280px] sm:rounded-2xl sm:block md:w-[300px] md:h-[300px] md:rounded-2xl md:block lg:w-[400px] lg:h-[400px] lg:rounded-2xl lg:block shadow-2xl shadow-zinc-500' />
-                </section>
-                <section>
-                    <div className='flex p-1'>
+        <div className='h-screen bg-[#f2f1f7] flex justify-center flex-col'>
+            <div className='w-auto text-black flex justify-center items-center text-center text-[25px] lg:text-[40px]'>
+                <Typewriter
+                    onInit={(typewriter) => {
+                        typewriter
+                            .typeString("Welcome to movie website.")
+                            .pauseFor(1000)
+                            .deleteAll()
+                            .typeString("Best movie platform.")
+                            .start();
+                    }}
+                />
+            </div>
 
-                        <h1 className='text-black'>{error && <h1 className='text-[10px] font-semibold'>{messageError.message}</h1>}</h1>
+            <div className='  p-2'>
+                <ToastContainer />
+                <main className='flex flex-col sm:flex-row md:flex-row lg:flex-row justify-center lg:items-center  gap-[5px] sm:gap-[10px] md:gap-[20px] lg:gap-[80px]'>
 
-                    </div>
-                    <div className='bg-white w-auto sm:w-[350px] md:w-[400px] lg:w-[600px] h-[380px] sm:h-[400px] md:h-[400px] lg:h-[400px] gap-1 flex flex-col items-center shadow-2xl rounded-[20px] lg:p-4 p-2 md:p-4'>
-                        <h1 className='text-[30px] sm:text-[35px] md:text-[38px] lg:text-[40px] text-center'>LogIn</h1>
-                        <button onClick={handleSignUp} className='cursor-pointer flex justify-between items-center text-[20px] border-black border-[1px] hover:bg-[#efefef]
+                    <section className='flex justify-center items-center'>
+                        <img src={logo} alt="logo" className='hidden sm:w-[280px] sm:h-[280px] sm:rounded-2xl sm:block md:w-[300px] md:h-[300px] md:rounded-2xl md:block lg:w-[400px] lg:h-[400px] lg:rounded-2xl lg:block shadow-2xl shadow-zinc-500' />
+                    </section>
+
+                    <section>
+                        <div className='flex p-1'>
+
+                            <h1 className='text-black'>{error && <h1 className='text-[10px] font-semibold'>{messageError.message}</h1>}</h1>
+                        </div>
+
+                        <div className='bg-white w-auto sm:w-[350px] md:w-[400px] lg:w-[600px] h-[380px] sm:h-[400px] md:h-[400px] lg:h-[400px] gap-1 flex flex-col items-center shadow-2xl rounded-[20px] lg:p-4 p-2 md:p-4'>
+
+                            <h1 className='text-[30px] sm:text-[35px] md:text-[38px] lg:text-[40px] text-center'>LogIn</h1>
+
+                            <button onClick={handleSignUp} className='cursor-pointer flex justify-between items-center text-[20px] border-black border-[1px] hover:bg-[#efefef]
             w-full  p-1 rounded-[4px]'>Google <FcGoogle className='text-[40px]' /></button>
-                        <h1 className='text-[25px]'>OR</h1>
-                        <form onSubmit={handleSignUpMicrosoft} className='flex flex-col gap-4 w-full '>
-                            <div className='flex justify-center items-center'>
 
-                                <input className='bg-transparent border-[1px] border-black p-2 rounded-[4px] text-[20px] w-full ' placeholder='Enter the Emmail...' type="email" name="" id="email" onChange={handleEmail} /> <MdOutlineEmail className='
+                            <h1 className='text-[25px]'>OR</h1>
+
+                            <form onSubmit={handleSignUpMicrosoft} className='flex flex-col gap-4 w-full '>
+                                <div className='flex justify-center items-center'>
+
+                                    <input className='bg-transparent border-[1px] border-black p-2 rounded-[4px] text-[20px] w-full ' placeholder='Enter the Emmail...' type="email" name="" id="email" onChange={handleEmail} /> <MdOutlineEmail className='
             border-[1px] border-black h-12 w-12 rounded-[4px] ' />
-                            </div>
+                                </div>
 
-                            <div className='flex justify-center items-center'>
+                                <div className='flex justify-center items-center'>
 
-                                <input className='bg-transparent border-[1px] border-black p-2 rounded-[4px] text-[20px] w-full ' placeholder='Enter the Password...' type={show ? "password" : "text"} name="" id="password" onChange={handlePassword} /> <IoKeyOutline className='
+                                    <input className='bg-transparent border-[1px] border-black p-2 rounded-[4px] text-[20px] w-full ' placeholder='Enter the Password...' type={show ? "password" : "text"} name="" id="password" onChange={handlePassword} /> <IoKeyOutline className='
             border-[1px] border-black h-12 w-12 rounded-[4px] ' onMouseEnter={handleShow} onMouseLeave={handleShow} />
-                            </div>
-                            {/* <input className='bg-transparent border-[1px] border-black p-2 rounded-[4px] text-[20px]' type="password" name="" id="password" onChange={handlePassword}/> */}
-                            <button type='submit' className='cursor-pointer text-[20px] border-black border-[1px] hover:bg-[#efefef]  w-full p-2 rounded-[4px]'>Login</button>
-                        </form>
-                        <h1 className='pt-2'>Create a New account <Link className='underline' to='/signin'>Signin</Link></h1>
-                    </div>
-                </section>
-            </main>
+                                </div>
+                                {/* <input className='bg-transparent border-[1px] border-black p-2 rounded-[4px] text-[20px]' type="password" name="" id="password" onChange={handlePassword}/> */}
+                                <button type='submit' className='cursor-pointer text-[20px] border-black border-[1px] hover:bg-[#efefef]  w-full p-2 rounded-[4px]'>Login</button>
+                            </form>
+
+                            <h1 className='pt-2'>Create a New account <Link className='underline' to='/signin'>Signin</Link></h1>
+
+                        </div>
+                    </section>
+                </main>
+            </div>
         </div>
     );
 }
